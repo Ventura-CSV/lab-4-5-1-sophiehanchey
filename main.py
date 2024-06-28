52dbf7e2fb9b27bf1b4fae81d94082931e9c5078
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 12:34:48 -0700

    initial code, pytest passed

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import random
 
 
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    total = 0
+    numbers = []
+    i = 0
+    
+    while i<5:
+        new_num = random.randint(0,100)
+        
+        numbers.append(new_num)
+        total += new_num
+        
+        i += 1
+        
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
